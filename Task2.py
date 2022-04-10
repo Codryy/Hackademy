d2777fb6c3976a3c0046d02cd28efdf98f4d7308
Codry <100705769+Codryy@users.noreply.github.com>
2022-04-10 21:35:24 +0300

    Delete Task2.py

--- Task2.py
+++
@@ -1,8 +0,0 @@
-#Task 2
-#Write a program that replaces the email adress
-
-mail=input("Introdu mail sa te furam: ")
-
-if '@gmail.com' in mail:
-    mail = mail.replace('gmail.com', 'nydailybugle.com')
-print(mail)
