f5dae948e35cc47eb37c773d0a21bbdbecfebeb3
Codry <100705769+Codryy@users.noreply.github.com>
2022-04-10 22:11:06 +0300

    Update Task8.py

--- Task8.py
+++ Task8.py
@@ -6,5 +6,5 @@ M=0
 while N:
     M=M*10+N%10
     N=N//10
-if M==C: print(C, "este palindrom")
-else: print(C, "nu este palindrom")
+if M==C: print(C, "is palindrome")
+else: print(C, "is not palindrome")
