1bfaee28501e6bafe48a969646daf4166cdae3fc
Codry <100705769+Codryy@users.noreply.github.com>
2022-04-10 22:57:19 +0300

    Update Task3.py

--- Task3.py
+++ Task3.py
@@ -1,9 +1,4 @@
 #Task3
-#Se citeste un număr natural ce reprezintă numărul de filme care au primit rating pe diverse site-uri de pe internet. 
-#Statistica rating-urilor este reprezentată în forma unui dicționar în care cheile reprezintă numele fimelor.
-#Valorile sunt reprezentate de câte o listă ce conține numere întregi pentru rating-urile filmului.
-#Definim rating global al unui film ca fiind media tuturor rating-urilor. 
-#Completați funcția global_ratings care va întoarce un dicționar ce are ca și cheie numele filmelor, iar valorile reprezintă rating-ul global al fiecăruia.
 
 def global_ratings(ratings):
     global_ratings = dict()
