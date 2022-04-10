b5d06a0e1d9da30349fd7a2bae46e56fa8f21b9c
Codry <100705769+Codryy@users.noreply.github.com>
2022-04-10 21:37:57 +0300

    Update Task4.py

--- Task4.py
+++ Task4.py
@@ -1,8 +1,4 @@
 #Task4
-#Se citește de la tastatură un număr N, si apoi N triplete de forma x y t. Să se afișeze pe o singură linie, în funcție de t, următoarele:
-#t = 'i' -> rezultatul împărțirii lui x la y ca numere întregi
-#t = 'f' -> rezultatul împărțirii lui x la y ca numere reale
-#t = 'p' -> rezultatul ridicării lui x la puterea y
 
 N = int(input())
 for i in range(0, N, 1):
