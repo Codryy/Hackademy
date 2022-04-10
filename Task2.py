cd15c2de9f4d1403565c9ee6ff95c862f1a44923
Codry <100705769+Codryy@users.noreply.github.com>
2022-04-10 22:43:10 +0300

    Update Task2.py

--- Task2.py
+++ Task2.py
@@ -1,11 +1,7 @@
 #task2
-#Fie o listă de numere naturale ce reprezintă costul unui bilet la cinematograf și un număr k de vouchere. 
-#Un voucher oferă o reducere de 100% pentru orice tip de bilet.
-#Să se completeze funcția minimum_cost ce returnează costul minim ce poate fi obținut după aplicarea a k vouchere.
 
 def minimum_cost(ticket_costs, k):
   min_cost = 0
-  max2=0
   for i in range(0,k):
       max1=0
       for j in ticket_costs:
@@ -16,6 +12,14 @@ def minimum_cost(ticket_costs, k):
   for i in ticket_costs:
       min_cost=min_cost+i
   return min_cost
-ticket_costs = list(map(int, input().split(' ')))
-k = int(input())
-print(minimum_cost(ticket_costs, k))
+
+#ticket_costs = list(map(int, input().split(' ')))
+#k = int(input())
+#print(minimum_cost(ticket_costs, k))
+
+ticket_costs1=[10, 20, 30, 100, 45]
+k1=3
+print("First example",minimum_cost(ticket_costs1, k1))
+ticket_costs2=[100, 200, 300]
+k2=5
+print("Second example",minimum_cost(ticket_costs2, k2))
